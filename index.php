18ab685d5885f96921b7b4522d997d9132d44e7d
Leticia <le.romero@outlook.com>
2018-09-11 18:14:15 +0000

    funcionando

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
          <?php get_header();?>
          
             <div class="row">
@@ -8,5 +7,32 @@
                     </div>
                 </div>
             </div>
+        
+        <div class="row">    
+        <?php
+        $cont = 0;
+            if (have_posts() ){
+                while (have_posts() ){
+                    the_post();
+                    $cont++;
+                if($cont <= 3){
+        ?>
+        
+                <div class="col-md-4">
+                    
+                    <h3><?php the_title(); ?></h3>
+                    <div class="img-responsive img-thumbnail foto"><?php the_post_thumbnail(); ?></div>
+                    <p><?php the_excerpt(); ?></p>
+                    <a class="btn btn-primary" href="<?php the_permalink();?>">Leia Mais</a>
+                    
+                </div>
+                
+        <?php
+                
+                } // fim do if do contador    
+                } //fim do while
+            } // fim do if
+        ?>
+        </div>
 
         <?php get_footer();?>
